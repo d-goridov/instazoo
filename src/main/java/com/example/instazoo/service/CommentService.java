afeafb13bb780adecc975e6200a1e450802c49c4
Dmitriy Goridov <dimagoridov@yandex.ru>
2023-08-23 19:02:05 +0300

    CommentService

---
+++ src/main/java/com/example/instazoo/service/CommentService.java
@@ -0,0 +1,70 @@
+package com.example.instazoo.service;
+
+import com.example.instazoo.dto.CommentDTO;
+import com.example.instazoo.entity.Comment;
+import com.example.instazoo.entity.Post;
+import com.example.instazoo.entity.User;
+import com.example.instazoo.exceptions.PostNotFoundException;
+import com.example.instazoo.repository.CommentRepository;
+import com.example.instazoo.repository.PostRepository;
+import com.example.instazoo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+
+    public static final Logger LOG = LoggerFactory.getLogger(PostService.class);
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Comment saveComment(Long postId, CommentDTO commentDTO, Principal principal) {
+        User user = getUserByPrincipal(principal);
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post cannot be found"));
+
+        Comment comment = new Comment();
+        comment.setPost(post);
+        comment.setId(commentDTO.getId());
+        comment.setMessage(comment.getMessage());
+        comment.setUsername(user.getUsername());
+
+        LOG.info("Saving comment for Post {}", post.getId());
+
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> getAllCommentsByPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException("Post cannot be found"));
+
+        return commentRepository.findAllByPost(post);
+    }
+
+    public void deleteComment(Long commentId) {
+        Optional<Comment> comment = commentRepository.findById(commentId);
+        comment.ifPresent(commentRepository::delete);
+    }
+
+
+    private User getUserByPrincipal(Principal principal) {
+        String username = principal.getName();
+        return userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found with username " + username));
+    }
+}
